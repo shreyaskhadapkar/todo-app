d47b90af9d4802cdc4a91e15c948c7d299ebee6f
KhadapkarS20 <shreyaskhadapkar@gmail.com>
2020-03-20 19:24:40 +0530

    Updates

---
+++ precache-manifest.bfe4db00bd04e0485705626387384bd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebad575ad1f2176a122fe5b4a0e541ef",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "94eb9265b516b4addc99",
+    "url": "/todo-app/static/css/main.01efac60.chunk.css"
+  },
+  {
+    "revision": "537e4a7dfdb2aded1684",
+    "url": "/todo-app/static/js/2.10db0911.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-app/static/js/2.10db0911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94eb9265b516b4addc99",
+    "url": "/todo-app/static/js/main.3d503f7b.chunk.js"
+  },
+  {
+    "revision": "aed0306b7a4838737999",
+    "url": "/todo-app/static/js/runtime-main.3c9e9223.js"
+  }
+]);
